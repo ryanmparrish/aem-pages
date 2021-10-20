87f18e00a6b0a27abbdbfa7cc249f77278672adc
Chris Millar <cmillar@adobe.com>
2021-10-20 14:21:05 -0600

    web test runner

--- blocks/header/tests/header.test.js
+++
@@ -1,5 +0,0 @@
-
-
-test('adds 1 + 2 to equal 3', () => {
-  expect([1,2,3].indexOf(4)).toBe(-1);
-});
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: [ 'js' ],
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-        '^.+\\.html?$': 'jest-raw-loader'
-    },
-    setupFilesAfterEnv: ["./jest.setup.js"]
-};
